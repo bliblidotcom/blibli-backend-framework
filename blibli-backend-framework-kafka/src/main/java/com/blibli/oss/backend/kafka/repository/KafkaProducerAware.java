ad78db887c4c8323a2e205b8f277186080d4c803
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-02-03 16:59:24 +0700

    use common lang for get fields reflection

--- blibli-backend-framework-kafka/src/main/java/com/blibli/oss/backend/kafka/repository/KafkaProducerAware.java
+++ blibli-backend-framework-kafka/src/main/java/com/blibli/oss/backend/kafka/repository/KafkaProducerAware.java
@@ -1,8 +1,9 @@
 package com.blibli.oss.backend.kafka.repository;
 
 import com.blibli.oss.backend.kafka.producer.KafkaProducer;
+import org.springframework.beans.factory.Aware;
 
-public interface KafkaProducerAware {
+public interface KafkaProducerAware extends Aware {
 
   void setKafkaProducer(KafkaProducer kafkaProducer);
 
