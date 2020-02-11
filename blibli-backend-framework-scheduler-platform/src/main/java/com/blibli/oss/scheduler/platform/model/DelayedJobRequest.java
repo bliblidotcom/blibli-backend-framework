a32e888dffeae408e6a6ce31daf08659b683754f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-02-11 23:24:55 +0700

    add scheduler platform base classes

---
+++ blibli-backend-framework-scheduler-platform/src/main/java/com/blibli/oss/scheduler/platform/model/DelayedJobRequest.java
@@ -0,0 +1,13 @@
+package com.blibli.oss.scheduler.platform.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DelayedJobRequest {
+}
