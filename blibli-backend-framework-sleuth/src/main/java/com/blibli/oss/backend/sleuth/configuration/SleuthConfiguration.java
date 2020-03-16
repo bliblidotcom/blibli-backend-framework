d785537638c423d54b005bb0d7f53d9255593476
eko.k.khannedy <eko.k.khannedy@gdn-commerce.com>
2020-03-16 17:41:58 +0700

    add sleuth module

---
+++ blibli-backend-framework-sleuth/src/main/java/com/blibli/oss/backend/sleuth/configuration/SleuthConfiguration.java
@@ -0,0 +1,8 @@
+package com.blibli.oss.backend.sleuth.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SleuthConfiguration {
+
+}
---
+++ blibli-backend-framework-sleuth/src/main/java/com/blibli/oss/backend/sleuth/package-info.java
@@ -0,0 +1,1 @@
+package com.blibli.oss.backend.sleuth;
