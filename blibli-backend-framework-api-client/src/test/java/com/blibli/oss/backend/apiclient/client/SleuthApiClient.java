e698832d449f019aef87616cc669a4a9f65c0605
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-06-08 12:28:37 +0700

    Support RequestParam with Collection (multiple values)

--- blibli-backend-framework-api-client/src/test/java/com/blibli/oss/backend/apiclient/client/SleuthApiClient.java
+++ blibli-backend-framework-api-client/src/test/java/com/blibli/oss/backend/apiclient/client/SleuthApiClient.java
@@ -1,11 +1,14 @@
 package com.blibli.oss.backend.apiclient.client;
 
 import com.blibli.oss.backend.apiclient.annotation.ApiClient;
+import com.blibli.oss.backend.apiclient.client.model.GenericResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Map;
 
 @ApiClient(
@@ -19,5 +22,11 @@ public interface SleuthApiClient {
     produces = MediaType.APPLICATION_JSON_VALUE
   )
   Mono<Map<String, String>> second();
+
+  @RequestMapping(
+    value = "/names",
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  Mono<GenericResponse<List<String>>> names(@RequestParam("names") List<String> names);
 
 }
